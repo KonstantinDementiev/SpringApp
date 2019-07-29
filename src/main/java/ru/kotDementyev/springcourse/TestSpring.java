a5481b96363c9f879aeb6f08d5119d857fa8b607
Konstantin Dementyev <dementyev@ukr.net>
2019-07-29 16:21:07 +0300

    Get prototyp, fabric method, init and destroy methods

--- src/main/java/ru/kotDementyev/springcourse/TestSpring.java
+++ src/main/java/ru/kotDementyev/springcourse/TestSpring.java
@@ -7,15 +7,38 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic2.getSong());
+
+        ClassicalMusic classicalMusic3 = context.getBean("classicalMusicBean1", ClassicalMusic.class);
+        System.out.println(classicalMusic3.getSong());
+
+
+
         //Music musicObj = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(musicObj);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//       boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
-        musicPlayer.playMusic();
-
-        System.out.println("Name: " + musicPlayer.getName());
-        System.out.println("Volume: " + musicPlayer.getVolume());
+//        System.out.println("Name: " + musicPlayer.getName());
+//        System.out.println("Volume: " + musicPlayer.getVolume());
 
         context.close();
     }
